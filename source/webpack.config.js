3786f13c904aea02fa25f9580d2fc8bc203a37bb
TheHllm <hllmollenkopf@gmail.com>
2021-01-22 18:09:41 +0100

    Added better build process

--- source/webpack.config.js
+++ source/webpack.config.js
@@ -2,50 +2,44 @@ const path = require('path');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
 
-module.exports = [
-  { //compile .ts and tsc
-    entry: './src/index.ts',
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts'],
-    },
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, '../dist'),
-    },
-    optimization: {
-      minimizer: [new UglifyJsPlugin()],
-    },
 
-  },
-  {
-    entry: ['./css/ide.css', './css/tgui.css', './css/codemirror.css', './css/documentation.css'],
-    mode: "production",
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-      ],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'bundle.css',
-      }),
+module.exports = {
+  entry: ['./src/index.ts', './css/ide.css', './css/tgui.css', './css/codemirror.css', './css/documentation.css'],
+  module: {
+    rules: [
+      // compile typescript
+      {
+        test: /\.tsx?$/i,
+        use: 'ts-loader',
+      },
+      // bundle css
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
     ],
-    output: {
-      path: path.resolve(__dirname, '../dist'),
-    },
   },
-
-];
+  resolve: {
+    extensions: ['.tsx', '.ts', '.css', '.html'],
+  },
+  plugins: [
+    // handle css
+    new MiniCssExtractPlugin(),
+    // create webpage
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      template: './index.html'
+    }),
+    // inline
+    new HtmlInlineScriptPlugin(),
+    new HTMLInlineCSSWebpackPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '_.js'
+  },
+};
