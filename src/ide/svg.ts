c9002705ce5dfc0710f46b646169c97fd246bfba
Manuel Fischer <58823087+manuel-fischer@users.noreply.github.com>
2021-09-27 17:29:55 +0200

    Visibility in SVGDrawingContext

--- src/ide/svg.ts
+++ src/ide/svg.ts
@@ -17,47 +17,38 @@ function createSvgElement(type, properties) {
 }
 
 export class SVGDrawingContext {
-	svg: SVGSVGElement;
-	classname: string;
-	style: string;
-	path?: string;
-    crisp: boolean;
+	public readonly svg: SVGSVGElement;
+	#classname: string;
+	#style: string;
+	#path?: string;
 
 	constructor(svg: SVGSVGElement) {
 		this.svg = svg;
-		this.classname = "";
-		this.style = "";
-        this.crisp = false;
-        //svg.setAttribute("shape-rendering", "geometricPrecision");
+		this.#classname = "";
+		this.#style = "";
 	}
 
 	setClass(classname: string): SVGDrawingContext {
-		this.classname = classname;
-		this.style = "";
+		this.#classname = classname;
+		this.#style = "";
 		return this;
 	}
 
 	setStyle(style: string): SVGDrawingContext {
-		this.classname = "";
-		this.style = style;
+		this.#classname = "";
+		this.#style = style;
 		return this;
 	}
 
 	setClassAndStyle(classname: string, style: string): SVGDrawingContext {
-		this.style = style;
-		this.classname = classname;
+		this.#style = style;
+		this.#classname = classname;
 		return this;
 	}
 
-    setCrisp(value: boolean): SVGDrawingContext {
-        this.crisp = value;
-        return this;
-    }
-
-	_addElement(el: SVGElement) {
-		if (this.classname) el.setAttribute("class", this.classname);
-		if (this.style) el.setAttribute("style", this.style);
-        if (this.crisp) el.setAttribute("shape-rendering", "crispEdges");
+	#addElement(el: SVGElement) {
+		if (this.#classname) el.setAttribute("class", this.#classname);
+		if (this.#style) el.setAttribute("style", this.#style);
 		this.svg.appendChild(el);
 		return this;
 	}
@@ -70,21 +61,21 @@ export class SVGDrawingContext {
 	): SVGDrawingContext {
 		let el = createSvgElement("rect", { x, y, width, height });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 
 	circle(cx: number, cy: number, r: number): SVGDrawingContext {
 		let el = createSvgElement("circle", { cx, cy, r });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 
 	line(x1: number, y1: number, x2: number, y2: number): SVGDrawingContext {
 		let el = createSvgElement("line", { x1, y1, x2, y2 });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 
@@ -101,33 +92,33 @@ export class SVGDrawingContext {
 		let points = this._accumulatePoints(points_args);
 		let el = createSvgElement("polygon", { points });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 	polyline(...points_args: Array<[number, number]>): SVGDrawingContext {
 		let points = this._accumulatePoints(points_args);
 		let el = createSvgElement("polyline", { points });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 
 	beginPath(): SVGDrawingContext {
-		this.path = "";
+		this.#path = "";
 		return this;
 	}
-	_appendPath(s: string) {
-		if (this.path) this.path += " ";
-		this.path += s;
+	#appendPath(s: string) {
+		if (this.#path) this.#path += " ";
+		this.#path += s;
 	}
 	// the following methods should be called between beginPath and endPath
 
 	moveTo(x: number, y: number): SVGDrawingContext {
-		this._appendPath(`M ${x} ${y}`);
+		this.#appendPath(`M ${x} ${y}`);
 		return this;
 	}
 	lineTo(x: number, y: number): SVGDrawingContext {
-		this._appendPath(`L ${x} ${y}`);
+		this.#appendPath(`L ${x} ${y}`);
 		return this;
 	}
 	ellipse(
@@ -155,9 +146,9 @@ export class SVGDrawingContext {
 		let ym = y + Math.sin((startAngle + endAngle) / 2) * ry;
 		let r = (rotation * 180) / Math.PI;
 
-		this._appendPath(`M ${x0},${y0}`);
-		this._appendPath(`A ${rx},${ry} ${r} 0 1 ${xm},${ym}`);
-		this._appendPath(`A ${rx},${ry} ${r} 0 1 ${x1},${y1}`);
+		this.#appendPath(`M ${x0},${y0}`);
+		this.#appendPath(`A ${rx},${ry} ${r} 0 1 ${xm},${ym}`);
+		this.#appendPath(`A ${rx},${ry} ${r} 0 1 ${x1},${y1}`);
 
 		//this._appendPath(`L ${x1},${y1}`);
 		return this;
@@ -174,15 +165,15 @@ export class SVGDrawingContext {
 	}
 
 	closePath(): SVGDrawingContext {
-		this._appendPath("Z");
+		this.#appendPath("Z");
 		return this;
 	}
 	endPath(): SVGDrawingContext {
-		let d = this.path;
-		this.path = undefined;
+		let d = this.#path;
+		this.#path = undefined;
 		let el = createSvgElement("path", { d });
 
-		this._addElement(el);
+		this.#addElement(el);
 		return this;
 	}
 }
