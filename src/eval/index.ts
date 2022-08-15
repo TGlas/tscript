6f595c16b8646f07931b5a65e8efbf3d44fdada7
Tobias Glasmachers <tobias.glasmachers@gmx.de>
2022-08-15 12:42:57 +0200

    rework of hasStructure

--- src/eval/index.ts
+++ src/eval/index.ts
@@ -1151,7 +1151,7 @@ export const evaluation = (function () {
 						}
 						if (code[pos] == "}") {
 							pos++;
-							ret.sub = sub.length == 1 ? sub[0] : sub;
+							ret.sub = sub.length === 1 ? sub[0] : sub;
 							return ret;
 						}
 						let cmd = command();
@@ -1180,7 +1180,8 @@ export const evaluation = (function () {
 					if (code[pos] == "}") {
 						pos++;
 						skipwhite();
-						if (pos >= code.length) return ret;
+						if (pos >= code.length)
+							return ret.length === 1 ? ret[0] : ret;
 						else return null;
 					}
 					let cmd = command();
@@ -1195,154 +1196,245 @@ export const evaluation = (function () {
 			}
 		}
 
-		// The function returns true if key is a sub-tree of data, and false otherwise.
-		// A tree is an arbitrary array or object. Non-array non-object nodes are ignored.
-		// Many member names like "parent" and "petype" are reserved. All other object members are considered child nodes.
-		// Two nodes match under the following conditions:
-		// - Arrays can match arrays; objects can match objects.
-		// - If at least one of the nodes has a petype member then both need one, and the key petype must be a substring of the data petype
-		// - If at least one of the nodes has a name member then both need one, and the key name must match the data name
-		// - Array members are considered in their natural order.
-		// - In the key tree, objects must have at most one child node, hence there is no order ambiguity.
+		//		// The function returns true if key is a sub-tree of data, and false otherwise.
+		//		// A tree is an arbitrary array or object. Non-array non-object nodes are ignored.
+		//		// Many member names like "parent" and "petype" are reserved. All other object members are considered child nodes.
+		//		// Two nodes match under the following conditions:
+		//		// - Arrays can match arrays; objects can match objects.
+		//		// - If at least one of the nodes has a petype member then both need one, and the key petype must be a substring of the data petype.
+		//		// - If at least one of the nodes has a name member then both need one, and the key name must match the data name.
+		//		// - Array members are considered in their natural order.
+		//		// - In the key tree, objects must have at most one child node, hence there is no order ambiguity.
+		let ignore = {
+			parent: true,
+			children: true,
+			superclass: true,
+			typedvalue: true,
+			names: true,
+			sim: true,
+			step: true,
+			types: true,
+			where: true,
+			breakpoints: true,
+		};
+		function isObject(value) {
+			return (
+				value !== null &&
+				typeof value == "object" &&
+				value.constructor == Object
+			);
+		}
+		let count = 0;
 		function isSubtree(key, data) {
-			function isObject(value) {
-				return (
-					value !== null &&
-					typeof value == "object" &&
-					value.constructor == Object
-				);
-			}
-
-			function isNode(value) {
-				return Array.isArray(value) || isObject(value);
-			}
-
-			function equal(c1, c2) {
-				if (
-					c1.nstack.length != c2.nstack.length ||
-					c1.dstack.length != c2.dstack.length ||
-					c1.next.length != c2.next.length
-				)
-					return false;
-				for (let i = 0; i < c1.nstack.length; i++) {
-					if (c1.nstack[i].length != c2.nstack[i].length)
-						return false;
-					for (let j = 0; j < c1.nstack[i].length; j++) {
-						if (c1.nstack[i][j] !== c2.nstack[i][j]) return false;
-					}
-				}
-				for (let i = 0; i < c1.dstack.length; i++) {
-					if (c1.dstack[i] !== c2.dstack[i]) return false;
-				}
-				for (let i = 0; i < c1.next.length; i++) {
-					if (c1.next[i] !== c2.next[i]) return false;
-				}
-				return true;
-			}
-
-			// initial cursor
-			let cs = [
-				{ nstack: new Array(), dstack: new Array(), next: [key] },
-			];
-
-			// recursive traversal of the data tree
-			function rec(node) {
-				if (!node) return false;
-				if (node.builtin) return false;
-				if (node.petype == "breakpoint") return false;
-
-				// forward pass: match nodes
-				let ii = cs.length;
-				for (let i = 0; i < ii; i++) {
-					let c = cs[i];
-					if (c.next.length == 0) continue; // this cursor is waiting for an ancestor of the current data node
-					let next = c.next[0];
-					if (!isNode(next))
-						throw new Error(
-							"[isSubtree] key node is not an array or object"
-						);
-
-					// do the nodes match?
-					let match = Array.isArray(node) == Array.isArray(next);
-					if (match && isObject(next)) {
-						if (next.hasOwnProperty("petype")) {
-							let d = node.petype ? node.petype : "";
-							let k = next.petype ? next.petype : "";
-							match = d.indexOf(k) >= 0;
+			// The key matches the data if
+			//  * isObject(key):
+			//     - the data object matches the key (petype, optionally base, optionally name), and sub-items match sub-items
+			//     - any child matches the key, recursively (array items and object attributes, irrespective of the name)
+			//  * Array.isArray(key):
+			//     - isObject(data): any data object attribute matches the key
+			//     - Array.isArray(data):
+			//       - a single array item matches the whole key
+			//       - each data item matches one key item, in the correct order
+			if (!Array.isArray(data) && !isObject(data)) return false;
+			if (isObject(key)) {
+				if (isObject(data)) {
+					if (data.builtin) return false;
+					if (data.hasOwnProperty("petype")) {
+						let match = data.petype === key.petype;
+						if (match && key.hasOwnProperty("base")) {
+							if (data.base !== key.base) match = false;
 						}
-						if (match && next.hasOwnProperty("name")) {
-							let d = node.name ? node.name : "";
-							let k = next.name ? next.name : "";
-							match = d == k;
+						if (match && key.hasOwnProperty("name")) {
+							if (data.name !== key.name) match = false;
+						}
+						if (match && key.hasOwnProperty("sub")) {
+							let match_sub = false;
+							for (let name in data) {
+								if (name in ignore) continue;
+								if (isSubtree(key.sub, data[name])) {
+									match_sub = true;
+									break;
+								}
+							}
+							if (!match_sub) match = false;
+						}
+						if (match) return true;
+					}
+					for (let name in data) {
+						if (name in ignore) continue;
+						if (isSubtree(key, data[name])) return true;
+					}
+				} else if (Array.isArray(data)) {
+					for (let i = 0; i < data.length; i++)
+						if (isSubtree(key, data[i])) return true;
+				}
+			} else if (Array.isArray(key)) {
+				if (isObject(data)) {
+					if (data.builtin) return false;
+					for (let name in data) {
+						if (name in ignore) continue;
+						if (isSubtree(key, data[name])) return true;
+					}
+				} else if (Array.isArray(data)) {
+					// try to match items one by one, possibly with gaps
+					let j = 0,
+						n = key.length;
+					for (let i = 0; i < data.length; i++) {
+						if (isSubtree(key[j], data[i])) {
+							j++;
+							if (j === n) return true;
 						}
 					}
 
-					if (match) {
-						// match found: create a new cursor
-						// - create a new next array from the children of the key node
-						// - add old next array with the first position removed and the data node to the stacks
-						let ns = c.nstack.slice();
-						let ds = c.dstack.slice();
-						ns.push(c.next.slice(1));
-						ds.push(node);
-						let nx = new Array();
-						for (let key in next) {
-							if (!next.hasOwnProperty(key)) continue;
-							if (reserved_node_names.hasOwnProperty(key))
-								continue;
-							let n = next[key];
-							if (isNode(n)) nx.push(n);
-						}
-						cs.push({ nstack: ns, dstack: ds, next: nx });
-					}
+					// try to match the whole key with a single data item
+					for (let i = 0; i < data.length; i++)
+						if (isSubtree(key, data[i])) return true;
 				}
-
-				// recursion
-				for (let key in node) {
-					if (!node.hasOwnProperty(key)) continue;
-					if (reserved_node_names.hasOwnProperty(key)) continue;
-					let n = node[key];
-					if (isNode(n)) {
-						if (rec(n)) return true;
-					}
-				}
-
-				// backward pass: collect results
-				let cs1 = new Array(),
-					cs2 = new Array();
-				for (let i = 0; i < cs.length; i++) {
-					let c = cs[i];
-					if (c.dstack[c.dstack.length - 1] == node) {
-						if (c.next.length > 0) continue; // sub-tree match not completed: drop the cursor
-
-						// move the cursor to the next sub-tree
-						c.dstack.pop();
-						c.next = c.nstack.pop();
-						if (c.nstack.length == 0) return true; // success!
-						cs2.push(c);
-					} else cs1.push(c);
-				}
-
-				// check for duplicate cursors
-				cs = cs1;
-				for (let i = 0; i < cs2.length; i++) {
-					let duplicate = false;
-					for (let j = 0; j < cs.length; j++) {
-						if (equal(cs2[i], cs[j])) {
-							duplicate = true;
-							break;
-						}
-					}
-					if (!duplicate) cs.push(cs2[i]);
-				}
-
-				return false;
-			}
-
-			return rec(data);
+			} else
+				throw new Error("[hasStructure] internal error in isSubtree");
+			return false;
 		}
 
+		//		function isSubtree(key, data) {
+		//			function isObject(value) {
+		//				return (
+		//					value !== null &&
+		//					typeof value == "object" &&
+		//					value.constructor == Object
+		//				);
+		//			}
+		//
+		//			function isNode(value) {
+		//				return Array.isArray(value) || isObject(value);
+		//			}
+		//
+		//			function equal(c1, c2) {
+		//				if (
+		//					c1.nstack.length != c2.nstack.length ||
+		//					c1.dstack.length != c2.dstack.length ||
+		//					c1.next.length != c2.next.length
+		//				)
+		//					return false;
+		//				for (let i = 0; i < c1.nstack.length; i++) {
+		//					if (c1.nstack[i].length != c2.nstack[i].length)
+		//						return false;
+		//					for (let j = 0; j < c1.nstack[i].length; j++) {
+		//						if (c1.nstack[i][j] !== c2.nstack[i][j]) return false;
+		//					}
+		//				}
+		//				for (let i = 0; i < c1.dstack.length; i++) {
+		//					if (c1.dstack[i] !== c2.dstack[i]) return false;
+		//				}
+		//				for (let i = 0; i < c1.next.length; i++) {
+		//					if (c1.next[i] !== c2.next[i]) return false;
+		//				}
+		//				return true;
+		//			}
+		//
+		//			// initial cursor
+		//			let cs = [
+		//				{ nstack: new Array(), dstack: new Array(), next: [key] },
+		//			];
+		//
+		//			// recursive traversal of the data tree
+		//			function rec(node) {
+		//				if (!node) return false;
+		//				if (node.builtin) return false;
+		//				if (node.petype == "breakpoint") return false;
+		//
+		//				// forward pass: match nodes
+		//				let ii = cs.length;
+		//				for (let i = 0; i < ii; i++) {
+		//					let c = cs[i];
+		//					if (c.next.length == 0) continue; // this cursor is waiting for an ancestor of the current data node
+		//					let next = c.next[0];
+		//					if (!isNode(next))
+		//						throw new Error(
+		//							"[isSubtree] key node is not an array or object"
+		//						);
+		//
+		//					// do the nodes match?
+		//					let match = Array.isArray(node) == Array.isArray(next);
+		//					if (match && isObject(next)) {
+		//						if (next.hasOwnProperty("petype")) {
+		//							let d = node.petype ? node.petype : "";
+		//							let k = next.petype ? next.petype : "";
+		//							match = d.indexOf(k) >= 0;
+		//						}
+		//						if (match && next.hasOwnProperty("name")) {
+		//							let d = node.name ? node.name : "";
+		//							let k = next.name ? next.name : "";
+		//							match = d == k;
+		//						}
+		//					}
+		//
+		//					if (match) {
+		//						// match found: create a new cursor
+		//						// - create a new next array from the children of the key node
+		//						// - add old next array with the first position removed and the data node to the stacks
+		//						let ns = c.nstack.slice();
+		//						let ds = c.dstack.slice();
+		//						ns.push(c.next.slice(1));
+		//						ds.push(node);
+		//						let nx = new Array();
+		//						for (let key in next) {
+		//							if (!next.hasOwnProperty(key)) continue;
+		//							if (reserved_node_names.hasOwnProperty(key))
+		//								continue;
+		//							let n = next[key];
+		//							if (isNode(n)) nx.push(n);
+		//						}
+		//						cs.push({ nstack: ns, dstack: ds, next: nx });
+		//					}
+		//				}
+		//
+		//				// recursion
+		//				for (let key in node) {
+		//					if (!node.hasOwnProperty(key)) continue;
+		//					if (reserved_node_names.hasOwnProperty(key)) continue;
+		//					let n = node[key];
+		//					if (isNode(n)) {
+		//						if (rec(n)) return true;
+		//					}
+		//				}
+		//
+		//				// backward pass: collect results
+		//				let cs1 = new Array(),
+		//					cs2 = new Array();
+		//				for (let i = 0; i < cs.length; i++) {
+		//					let c = cs[i];
+		//					if (c.dstack[c.dstack.length - 1] == node) {
+		//						if (c.next.length > 0) continue; // sub-tree match not completed: drop the cursor
+		//
+		//						// move the cursor to the next sub-tree
+		//						c.dstack.pop();
+		//						c.next = c.nstack.pop();
+		//						if (c.nstack.length == 0) return true; // success!
+		//						cs2.push(c);
+		//					} else cs1.push(c);
+		//				}
+		//
+		//				// check for duplicate cursors
+		//				cs = cs1;
+		//				for (let i = 0; i < cs2.length; i++) {
+		//					let duplicate = false;
+		//					for (let j = 0; j < cs.length; j++) {
+		//						if (equal(cs2[i], cs[j])) {
+		//							duplicate = true;
+		//							break;
+		//						}
+		//					}
+		//					if (!duplicate) cs.push(cs2[i]);
+		//				}
+		//
+		//				return false;
+		//			}
+		//
+		//			return rec(data);
+		//		}
+
 		let pc = compilePseudo(pseudo);
+		console.log("[compilePseudo]", pseudo, pc);
 		if (pc === null) {
 			console.log("error in pseudo code:\n\n" + pseudo);
 			return false;
