e0a182cdcdc5558a5115f08026d506cc1ba212eb
Paul Wiegand <pwiegand2001@gmail.com>
2025-07-24 13:41:19 +0200

    Add config info

--- src/github_creds.ts
+++ src/github_creds.ts
@@ -1,10 +1,12 @@
-// url of git proxy server
+// url of git proxy server (including ports)
 export const server_url = "http://localhost:7000";
 export const proxy_server_url = "http://localhost:7001";
 
 // GitHub specific
+// This is the client ID that is issued by GitHub when creating a new OAuth application.
 export const client_id_github = "Ov23li79J7jZEgnG3sdL";
 
 // GitLab specific
+// This is the client ID (GitLab calls it application ID) that is issued by GitLab when creating a new OAuth application.
 export const app_id_gitlab =
 	"701a61d3f73375e0c4ccf3c07950b75c766e2c40a944a9fe0015079feb730140";
