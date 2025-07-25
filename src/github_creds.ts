955bb562d607d917a88cf70834ef4685eb9a6d68
Paul Wiegand <pwiegand2001@gmail.com>
2025-07-25 12:20:52 +0200

    Update Git credentials

--- src/github_creds.ts
+++ src/github_creds.ts
@@ -1,6 +1,6 @@
 // url of git proxy server (including ports)
-export const server_url = "http://localhost:7000";
-export const proxy_server_url = "http://localhost:7001";
+export const server_url = "http://togla.ini.rub.de:31234";
+export const proxy_server_url = "http://togla.ini.rub.de:31235";
 
 // GitHub specific
 // This is the client ID that is issued by GitHub when creating a new OAuth application.
@@ -9,4 +9,4 @@ export const client_id_github = "Ov23li79J7jZEgnG3sdL";
 // GitLab specific
 // This is the client ID (GitLab calls it application ID) that is issued by GitLab when creating a new OAuth application.
 export const app_id_gitlab =
-	"701a61d3f73375e0c4ccf3c07950b75c766e2c40a944a9fe0015079feb730140";
+	"72f4e1d2b9f44a1d54d4b3614147b9d3b73b02c9c20d5af59249058f5d4ab102";
